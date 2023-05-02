489549a6d317248bf0e6ab1d857e647875252727
Samuel Enoque <thesonlike@gmail.com>
2023-05-02 17:57:48 -0300

    beforeDebugging

--- vite-project/src/App.js
+++ vite-project/src/App.js
@@ -1,13 +1,12 @@
 import logo from 'C:\Users\PC\Desktop\firebasewithvuiton\vite-project\src\logo.svg';//better not to risk using the vite svg
 import './App.css';
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "C:\Users\PC\Desktop\firebasewithvuiton\vite-project\components\NavBar.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
         <NavBar />
-        <Banner />
     </div>
  );
 }
